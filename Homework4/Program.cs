3e0aeb2aaac121df68fb8121db6f7d773e2fcc0c
Artem <artkruzhkov@gmail.com>
2022-09-15 22:40:50 +0300

    Task4

---
+++ Homework4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("enter number");
+int n = int.Parse(Console.ReadLine());
+int x = 2;
+
+while(x <= n)
+{
+    if(x % 2 == 0)
+    {
+        Console.Write(x+" ");
+
+    }
+    x++;
+}
