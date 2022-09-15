ece328ff9bfabef097149c06888e49dff26eee9d
Artem <artkruzhkov@gmail.com>
2022-09-15 22:15:35 +0300

    Task2

---
+++ Homework2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("enter first number");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("enter second number");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("enter third number");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.WriteLine($"max = {max}");
