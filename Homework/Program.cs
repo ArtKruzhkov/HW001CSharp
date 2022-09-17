04aed46ec69039b679835ab15bbea34bb5a4b09e
Artem <artkruzhkov@gmail.com>
2022-09-17 18:35:13 +0300

    added minmax

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -5,7 +5,9 @@ Console.WriteLine("enter second number");
 int b = int.Parse(Console.ReadLine()!);
 
 int max = a;
+int min = b;
 
 if(b > max) max = b;
+if(a < min) min = a;
 
-Console.WriteLine($"max = {max}");
+Console.WriteLine($"max = {max} min = {min}");
